89ec1974d4074a2b3a1837a9c92d44161913b224
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-10-13 20:03:36 -0300

    Atividade Prátida TDD - Base Projeto

---
+++ Laboratório de Desenvolvimento em Banco de Dados V/praticaTDD/src/main/java/edu/fatec/praticaTDD/PraticaTddApplication.java
@@ -0,0 +1,13 @@
+package edu.fatec.praticaTDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaTddApplication.class, args);
+	}
+
+}
---
+++ Laboratório de Desenvolvimento em Banco de Dados V/praticaTDD/src/test/java/edu/fatec/praticaTDD/PraticaTddApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fatec.praticaTDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
